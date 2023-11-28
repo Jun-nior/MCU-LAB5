1e03a18babab5f2c23dd56d8e3bdbb0ec168e32c
Jun-nior <petertg1702@gmail.com>
2023-11-28 23:41:52 +0700

    Update lab 4 into scheduler

--- STM/Core/Src/scheduler.c
+++ STM/Core/Src/scheduler.c
@@ -7,7 +7,7 @@
 
 #include "scheduler.h"
 #include "main.h"
-
+#include <stdio.h>
 int global_time=0;
 
 UART_HandleTypeDef huart2;
@@ -27,6 +27,7 @@ void SCH_Update(void) {
 	unsigned char index;
 	for (index=0;index<SCH_MAX_TASKS;index++) {
 		 if (SCH_tasks_G[index].pTask && SCH_tasks_G[index].Delay==0) {
+//			timestamp(index);
 			SCH_tasks_G[index].RunMe+=1;
 			if (SCH_tasks_G[index].Period) {
 				SCH_tasks_G[index].Delay=SCH_tasks_G[index].Period-1;
@@ -45,8 +46,9 @@ uint32_t SCH_Add_Task(void (*pFunction)(), uint32_t DELAY, uint32_t PERIOD) {
 	if (index>=SCH_MAX_TASKS) {
 		return -1;
 	}
+	DELAY=(DELAY==0)?0:DELAY-1;
 	SCH_tasks_G [ index ].pTask = pFunction;
-	SCH_tasks_G [ index ].Delay = (DELAY == 0)?0:DELAY-1;
+	SCH_tasks_G [ index ].Delay = DELAY ;
 	SCH_tasks_G [ index ].Period = PERIOD ;
 	SCH_tasks_G [ index ].RunMe = 0 ;
 	SCH_tasks_G [ index ].TaskID= index;
@@ -59,12 +61,11 @@ void SCH_Dispatch_Task(void) {
 	for (index=0;index<SCH_MAX_TASKS;index++) {
 		if (SCH_tasks_G[index].RunMe>0) {
 			(*SCH_tasks_G[index].pTask)();
-//			HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 			SCH_tasks_G[index].RunMe-=1;
 			if (SCH_tasks_G[index].Period==0 && SCH_tasks_G[index].RunMe == 0 && SCH_tasks_G[index].Delay==0) {
 				SCH_Delete_Task(index);
 			}
-			timestamp(index);
+//			timestamp(index);
 		}
 
 	}
